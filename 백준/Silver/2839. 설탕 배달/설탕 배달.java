711144bb779a47bbd6d86503b9d89c263ad2f5ca
2kihyuk <115782138+2kihyuk@users.noreply.github.com>
2025-07-22 20:45:48 +0900

    [Silver IV] Title: 설탕 배달, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,24 +1,35 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)  {
-        Scanner sc = new Scanner (System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
 
-        if(N ==4 || N==7){
-            System.out.println(-1);
-        }
-        else if(N%5==0){
-            System.out.println(N/5);
-        }
-        else if(N%5==1 || N%5==3){
-            System.out.println((N/5)+1);
-        }
-        else if(N%5==2 || N%5==4){
-            System.out.println((N/5)+2);
+        int[]dp = new int[N+1];
+
+        Arrays.fill(dp,5001);
+        dp[0] = 0;
+
+        for(int i =1; i<=N;i++){
+            if(i >=3){
+                if(dp[i-3] != 5001){
+                    dp[i] = Math.min(dp[i],dp[i-3] + 1);
+                }
+            }
+            if(i >= 5){
+                if(dp[i-5] != 5001){
+                    dp[i] = Math.min(dp[i], dp[i-5] + 1);
+                }
+            }
         }
 
+        if(dp[N] == 5001){
+            System.out.println(-1);
+        }else{
+            System.out.println(dp[N]);
+        }
     }
 }
