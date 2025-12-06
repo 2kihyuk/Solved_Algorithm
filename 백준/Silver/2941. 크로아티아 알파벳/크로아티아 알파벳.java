39c5a53d10418616c8dc5ebc24f9647cc0ad5836
2kihyuk <115782138+2kihyuk@users.noreply.github.com>
2025-12-06 18:12:09 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -2,46 +2,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    private static String[] arr = {"c=","c-","dz=","d-","lj","nj","s=","z="};
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String line = br.readLine();
 
-        int cnt = 0;
+        // 순서 중요: dz= 이 z= 보다 먼저 와야 함
+        String[] cro = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
-        int len = line.length();
-        int idx = 0;
-
-        while(idx < len){
-
-            if(idx+2 < len && line.charAt(idx) == 'd' && line.charAt(idx+1) == 'z' && line.charAt(idx+2) == '='){
-                cnt++;
-                idx+=3;
-                continue;
-            }else if(idx +1 < len){
-                String two = line.substring(idx,idx+2);
-                if (two.equals("c=") || two.equals("c-") ||
-                        two.equals("d-") || two.equals("lj") ||
-                        two.equals("nj") || two.equals("s=") ||
-                        two.equals("z=")){
-                    cnt++;
-                    idx+=2;
-                    continue;
-                }else{
-                    cnt++;
-                    idx++;
-                    continue;
-                }
-            }
-            else{
-                cnt++;
-                idx++;
-            }
+        for (String c : cro) {
+            line = line.replace(c, "*");
         }
 
-        System.out.println(cnt);
-
+        System.out.println(line.length());
     }
 }
